79191556903095139635ce5db09487119e416167
Alberto Elorza <albertoelorza2002@gmail.com>
2022-10-20 14:13:38 +0200

    app main structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MapModule } from './components/map/map.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
