2c9f0bf00b7434745faddfef9405d14c4bbec085
Alberto Elorza <albertoelorza2002@gmail.com>
2022-10-21 16:48:20 +0200

    artifacts is a observable

--- src/app/shared/interfaces/artifacts.ts
+++ src/app/shared/interfaces/artifacts.ts
@@ -1,34 +1,12 @@
 import { Observable } from "rxjs";
 
-export interface ArtifactEvent {
-  /**
-   * Artifact unique identifier
-   */
-  id: number;
-  /**
-   * Artifact name
-   */
-  name: string;
-  /**
-   * Artifact type name
-   */
-  type: string;
-  event: 'UP' | 'DOWN' | 'NOTIFY' | 'UPDATE';
-  location: [number, number];
-  data: any | undefined;
-  /**
-   * Only Available on UP and UPDATE
-   */
-  actions: string[] | undefined;
-}
-
-export abstract class ArtifactManager {
-  abstract listen(): Observable<ArtifactEvent>
-}
-
 export interface Artifact {
   id: number;
   name: string;
   location: [number, number];
   imageURL: string | null;
+}
+
+export abstract class ArtifactManager {
+  abstract artifacts(): Observable<Artifact[]>;
 }
